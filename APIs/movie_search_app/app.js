8c80a38158ff0bc1b1c7ac6ef9ddcf4d09be0970
Younghoon Jo <edwardjo811@gmail.com>
2017-08-28 15:35:26 +0000

    modify movieApp

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.static(__dirname + '/views'));
 
 app.get("/", function(req, res) {
    res.render("search"); 
